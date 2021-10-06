ebf6b057a2d4b9139e8b5be906fa5f6b284c1e20
MACP2503 <74038341+MACP2503@users.noreply.github.com>
2021-10-06 17:10:45 -0500

    Avance 2
    Nuevos archivos y algunos solamente actualizados

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-#include <conio.h>
-#include<algorithm>
+#include <math.h>
+#include <algorithm>
+#include "dlist.h"
 #define NOMBRE_EXCEL "Tabla_Harris-Benedict.csv"
+#define HORAS "horas.txt"
+#define DIAS "dias.txt"
 
 using namespace std;
 
@@ -114,6 +117,191 @@ else if (e==5){
 return tmb;
 }
 
+void imprimirlista(){
+    cout<<"Los horarios de citas agendadas son: "<<endl<<endl;
+    string line;
+    vector<string> di, ho;
+    char coma = ',';
+    ifstream file (HORAS);
+    ifstream file2 (DIAS);
+    int i = 0;
+
+    while(!file.eof()){
+    getline(file,line,coma);
+    ho.push_back(line);
+    }
+    file.close();
+
+    while (!file2.eof()){
+    getline(file2,line,coma);
+    di.push_back(line);
+    }
+    file2.close();
+
+    while(i<=di.size()){
+    if(i<di.size()){
+      cout<<"Cita "<<i+1<<": ";
+    }
+    else{
+        break;
+    }
+    double h, d = 0;
+    int h1, d1 = 0;
+    h=stoi(ho[i]);
+    d=stoi(di[i]);
+    h1 = (trunc(h)/100);
+    d1 = (trunc(d)/100);
+    h = h/100;
+    h= (h-h1)*100;
+    d = d/100;
+    d= (d-d1)*100;
+
+    cout<<"Hora: "<<h1<<":"<<h;
+    if(h==0){
+        cout<<0<<endl;
+    }
+    else{
+        cout<<endl;
+    }
+
+    cout<<"Dia "<<d1<<" del mes numero "<<d<<endl<<endl;
+    i++;
+}
+
+}
+
+void lista(){
+    string line;
+    char coma = ',';
+    ifstream file (HORAS);
+    ifstream file2 (DIAS);
+    DList<int> hora, dia;
+
+    while(!file.eof()){
+    getline(file,line,coma);
+    hora.insertion(stoi(line));
+    }
+    file.close();
+
+    while (!file2.eof()){
+    getline(file2,line,coma);
+    dia.insertion(stoi(line));
+    }
+    file2.close();
+
+    int decision = 1;
+    while(decision!=0){
+    int dec;
+    imprimirlista();
+    cout<<"Que desea hacer?"<<endl;
+    cout<<"1. Agregar Cita"<<endl;
+    cout<<"2. Modificar Cita"<<endl;
+    cout<<"3. Eliminar Cita"<<endl;
+    cout<<"4. Salir del menu de citas"<<endl;
+    cin>>dec;
+    while((dec<=0)||(dec>4)){
+    cout<<"Digite un numero valido: "<<endl;
+    cout<<"Que desea hacer?"<<endl;
+    cout<<"1. Agregar Cita"<<endl;
+    cout<<"2. Modificar Cita"<<endl;
+    cout<<"3. Eliminar Cita"<<endl;
+    cout<<"4. Salir del menu de citas"<<endl;
+    cin>>dec;
+    }
+    if(dec==4){
+        cout<<endl;
+        break;
+    }
+    else if(dec==1){
+        int horaingresada, minutosingresados, horamin;
+        cout<<"Digita la hora en un numero del 1 al 24: ";
+        cin>>horaingresada;
+        while((horaingresada<=0)||(horaingresada>24)){
+            cout<<"Digita la hora en un numero del 1 al 24: ";
+            cin>>horaingresada;
+        }
+        cout<<"Digita los minutos dentro de la hora en un numero del 0 al 59: ";
+        cin>>minutosingresados;
+        while((minutosingresados<0)||(minutosingresados>59)){
+        cout<<"Digita los minutos dentro de la hora en un numero del 0 al 59: ";
+        cin>>minutosingresados;
+        }
+        horamin=(horaingresada*100)+minutosingresados;
+        hora.insertion(horamin);
+
+        int diaingresado, mesingresado, diames;
+        cout<<"Digita el dia en un numero del 1 al 31: ";
+        cin>>diaingresado;
+        while((diaingresado<=0)||(diaingresado>31)){
+            cout<<"Digita la hora en un numero del 1 al 31: ";
+            cin>>diaingresado;
+        }
+        cout<<"Digita el mes en un numero del 1 al 12: ";
+        cin>>mesingresado;
+        while((mesingresado<=0)||(mesingresado>12)){
+        cout<<"Digita el mes en un numero del 1 al 12: ";
+        cin>>mesingresado;
+        }
+        diames=(diaingresado*100)+mesingresado;
+        dia.insertion(diames);
+    }
+    else if(dec==2){
+        int nocita;
+        cout<<"Que numero de cita desea modificar: "<<endl;
+        cin>>nocita;
+        nocita=nocita-1;
+
+        int horaingresada, minutosingresados, horamin;
+        cout<<"Digita la hora en un numero del 1 al 24: ";
+        cin>>horaingresada;
+        while((horaingresada<=0)||(horaingresada>24)){
+            cout<<"Digita la hora en un numero del 1 al 24: ";
+            cin>>horaingresada;
+        }
+        cout<<"Digita los minutos dentro de la hora en un numero del 0 al 59: ";
+        cin>>minutosingresados;
+        while((minutosingresados<0)||(minutosingresados>59)){
+        cout<<"Digita los minutos dentro de la hora en un numero del 0 al 59: ";
+        cin>>minutosingresados;
+        }
+        horamin=(horaingresada*100)+minutosingresados;
+        hora.update(nocita, horamin);
+
+        int diaingresado, mesingresado, diames;
+        cout<<"Digita el dia en un numero del 1 al 31: ";
+        cin>>diaingresado;
+        while((diaingresado<=0)||(diaingresado>31)){
+            cout<<"Digita la hora en un numero del 1 al 31: ";
+            cin>>diaingresado;
+        }
+        cout<<"Digita el mes en un numero del 1 al 12: ";
+        cin>>mesingresado;
+        while((mesingresado<=0)||(mesingresado>12)){
+        cout<<"Digita el mes en un numero del 1 al 12: ";
+        cin>>mesingresado;
+        }
+        diames=(diaingresado*100)+mesingresado;
+        dia.update(nocita, diames);
+    }
+    else if(dec==3){
+        int nocita;
+        cout<<"Que numero de cita desea eliminar: "<<endl;
+        cin>>nocita;
+        nocita=nocita-1;
+        dia.deleteAt(nocita);
+        hora.deleteAt(nocita);
+    }
+
+    ofstream file5(HORAS);
+    file5<<hora.toStringForward();
+    file5.close();
+
+    ofstream file4(DIAS);
+    file4<<dia.toStringForward();
+    file4.close();
+    }
+}
+
 void imprimir(vector<vector<string>> vector_final){
 int i;
 for(i=0;i<vector_final.size();i++){
@@ -154,16 +342,18 @@ int sel;
 cout<<"Calorex"<<endl;
 cout<<"1: Ordena los pacientes por consumo calorico ascendente."<<endl;
 cout<<"2: Ordena los pacientes por orden alfabetico."<<endl;
-cout<<"3: Salir."<<endl;
+cout<<"3: Agenda citas en la lista."<<endl;
+cout<<"4: Salir."<<endl;
 cout<<"Seleccione el numero de una opcion a ejecutar: "<<endl;
 cin>>sel;
 cout<<endl;
 
-while((sel<=0)||(sel>3)){
+while((sel<=0)||(sel>4)){
 cout<<"Seleccione un digito correcto."<<endl;
 cout<<"1: Ordena los pacientes por consumo calorico ascendente."<<endl;
 cout<<"2: Ordena los pacientes por orden alfabetico."<<endl;
-cout<<"3: Salir."<<endl;
+cout<<"3: Agenda citas en la lista."<<endl;
+cout<<"4: Salir."<<endl;
 cout<<"Seleccione el numero de una opcion a ejecutar: "<<endl;
 cin>>sel;
 cout<<endl;
@@ -179,9 +369,14 @@ if (sel == 2){
     ordenaporalfabeto(s);
     seleccion(s);
 }
+
 if (sel == 3){
+    lista();
+    seleccion(s);
+}
+
+if (sel == 4){
     cout<<"Muchas gracias por usar Calorex! Vuelva pronto!"<<endl;
-    cout<<"Pulsa cualquier tecla para cerrar la plataforma"<<endl;
 }
 
 }
@@ -215,8 +410,6 @@ int main()
     }
     cout<<"Bienvenid@ a la plataforma nutricional Calorex! Existen "<<vector_pacientes.size()<<" pacientes en la plataforma los cuales son: "<<endl;
     imprimir(vector_pacientes);
-    seleccion(vector_pacientes);
-
     archivo.close();
-    getch();
+    seleccion(vector_pacientes);
 }
